8b0ccb02fb2ef7fd9c8527ec197f16ab5abfffa7
amikesalumni <amikes.alumni.nd.edu>
2020-07-15 15:15:09 -0400

    Procfile modded

--- pythoncode.py
+++ pythoncode.py
@@ -72,7 +72,4 @@ def create_app():
         root.destroy()
         return 'folder changed'
 
-    if __name__ == '__main__':
-        webbrowser.open('http://127.0.0.1:5000/', new=2)
-        app.run()
     return app
